2800b5cd26e90bb3a790844bb4cf67feecb5efc0
nick-Sutton <136986806+nick-Sutton@users.noreply.github.com>
2023-12-31 14:58:08 -0500

    re-format

--- Color Detection/colordetection.py
+++ Color Detection/colordetection.py
@@ -1,24 +1,28 @@
 import cv2, numpy
-cap = cv2.VideoCapture(0)
+def main():
+    cap = cv2.VideoCapture(0)
 
-if not cap.isOpened():
-    print("Cannot open camera")
-    exit()
+    if not cap.isOpened():
+        print("Cannot open camera")
+        exit()
 
-while True:
-    ret, frame = cap.read()
+    while True:
+        ret, frame = cap.read()
 
-    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+        hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
-    lower_purple = numpy.array([90, 50, 50])
-    upper_purple = numpy.array([130, 255, 255])
+        lower_purple = numpy.array([90, 50, 50])
+        upper_purple = numpy.array([130, 255, 255])
 
-    mask = cv2.inRange(hsv, lower_purple, upper_purple)
-    results = cv2.bitwise_and(frame, frame, mask=mask)
+        mask = cv2.inRange(hsv, lower_purple, upper_purple)
+        results = cv2.bitwise_and(frame, frame, mask=mask)
 
-    cv2.imshow('frame', results)
-    if cv2.waitKey(1) == ord('q'):
-        break
+        cv2.imshow('frame', results)
+        if cv2.waitKey(1) == ord('q'):
+            break
 
-cap.release()
-cv2.destroyAllWindows()
+    cap.release()
+    cv2.destroyAllWindows()
+
+if __name__ == "__main__":
+    main()
